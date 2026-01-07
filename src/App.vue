47b99fc9ef4a6c6785f8a55447c31abf83eedd34
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2026-01-07 20:43:00 +1100

    domain ranking initialised

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-extralight text-green-400">Domain rank</h1>
+    <h1 class="text-3xl font-extralight text-red-400">
+      tailwind working for sure
+    </h1>
+  </div>
+</template>
